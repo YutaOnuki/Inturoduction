ca525f7123a82e86b54873cdde184f28904cd9f5
Yuta Onuki <onu4648yu@icloud.com>
2018-10-29 12:28:48 +0000

    メール微調整

--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
 
-  # Preview this email at http://68416231059c4cf399502cad93064c20.vfs.cloud9.us-east-2.amazonaws.com/mailers/contact_mailer/contact_mail
+  # Preview this email at http://68416231059c4cf399502cad93064c20.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/contact_mailer/contact_mail
   def contact_mail
     contact = Contact.first
     ContactMailer.contact_mail(contact)
